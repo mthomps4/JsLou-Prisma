0f33cfc48ba14dbed3b3b76507e1a29499c15511
Matt Thompson <matthew.thompson.a@gmail.com>
2019-03-10 15:01:42 -0400

    cleanup

--- docker-scripts/entrypoint.sh
+++ docker-scripts/entrypoint.sh
@@ -9,11 +9,11 @@ if [ $DEBUG = 1 ]; then
 
   echo "Running in debug mode"
 
-  nodemon src/index.js --ext js,graphql --exec babel-node
+  # nodemon src/index.js --ext js,graphql --exec babel-node
 
-  # nodemon --exec 'prisma deploy && node' src/index.js --delay 2.5 &
+  nodemon --ext js,graphql --exec 'prisma deploy && babel-node' src/index.js --delay 2.5 &
 
-  # graphql playground
+  graphql playground
 
 else
 

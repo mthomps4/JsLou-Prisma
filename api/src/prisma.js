f00cdda36320b4ec0c6eca0cbfe45fa8d60b74c4
Matt Thompson <matt@echobind.com>
2019-03-19 13:45:52 -0400

    docker 2.0

--- api/src/prisma.js
+++ api/src/prisma.js
@@ -1,7 +1,7 @@
 import { Prisma } from 'prisma-binding';
 
 const prisma = new Prisma({
-  typeDefs: 'src/generated/prisma.graphql',
+  typeDefs: 'api/src/generated/prisma.graphql',
   endpoint: 'http://localhost:4466',
   secret: process.env.REACT_APP_PRISMA_SECRET,
   debug: process.env.DEBUG === 1
